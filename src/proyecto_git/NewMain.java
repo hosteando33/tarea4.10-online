bf3947210cfb9fe913a43d71784207874647f600
Daniel <62617940+hosteando33@users.noreply.github.com>
2022-03-07 11:45:22 +0100

    init git

---
+++ src/proyecto_git/NewMain.java
@@ -0,0 +1,7 @@
+
+package proyecto_git;
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar (15f,4f));
+    }
+}
---
+++ src/proyecto_git/Sumador.java
@@ -0,0 +1,8 @@
+
+package proyecto_git;
+
+public class Sumador {
+    public static float sumar (float a, float b ) {
+        return  a+b;
+    }
+} 
