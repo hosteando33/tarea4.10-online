dc2afc4996f94f800805420ff551d50f8c103334
Daniel <62617940+hosteando33@users.noreply.github.com>
2022-03-07 12:02:18 +0100

    modificacion a Restador

---
+++ src/proyecto_git/Restador.java
@@ -0,0 +1,14 @@
+/*
+ * @alumno: Daniel Garcia Martinez
+ * @curso: Programacion
+ * @fecha: 02-2021
+ * @Tema: Tema 12
+ * @Ejercicio: 12.
+ * @Enunciado:
+ */
+package proyecto_git;
+
+public class Restador {
+    public static float restar (float a, float b ){
+    return   a - b;
+}} 
